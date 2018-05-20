6a7af3eaa314bac50f42d4247532dee2dbcd6556
choke <chris.hoke@billboard.com>
2018-05-20 15:04:27 -0500

    added handleError function to authservice, cleaned up nav html

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +14,46 @@ export class AuthService {
   constructor(private http: Http) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).pipe(map((response: Response) => {
-      const user = response.json();
-      if (user) {
-        localStorage.setItem('token', user.value);
-        this.userToken = user.value;
-      }
-    }));
+    return this.http.post(this.baseUrl + 'login', model, this.requestOptions())
+      .pipe(
+        map((response: Response) => {
+          const user = response.json();
+          if (user) {
+            localStorage.setItem('token', user.value);
+            this.userToken = user.value;
+          }
+        }),
+        catchError(this.handleError)
+      );
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).pipe(catchError(this.handleError));
   }
 
   private requestOptions() {
     const headers = new Headers({'Content-type': 'application/json'});
     return new RequestOptions({headers: headers});
+  }
+
+  private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+    if (applicationError) {
+      return throwError(error);
+    }
+
+    const serverError = error.json();
+    let modelStateErrors = '';
+
+    if (serverError) {
+      for (const key in serverError) {
+        if (serverError[key]) {
+          modelStateErrors += serverError[key] + '\n';
+        }
+      }
+    }
+    return throwError(
+      modelStateErrors || 'server error'
+    );
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
       console.log('registration success');
     },
     error => {
-      console.log('registration failure');
+      console.log(error);
     });
   }
 
